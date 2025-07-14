3b41a0e71dd3123ac32a319ec288168f09c1ff04
Abhishek Verma <114013340+abhishekv1000@users.noreply.github.com>
2025-07-14 13:47:46 +0530

    Update ProductsMenu.jsx

--- Client/src/pages/customer/components/ProductsMenu.jsx
+++ Client/src/pages/customer/components/ProductsMenu.jsx
@@ -3,28 +3,24 @@ import { styled, alpha } from '@mui/material/styles';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
-import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material'
+import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
 import { LightPurpleButton } from '../../../utils/buttonStyles';
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts, getSearchedProducts } from '../../../redux/userHandle';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const ProductsMenu = ({ dropName }) => {
-
-    const navigate = useNavigate()
-
+    const navigate = useNavigate();
     const dispatch = useDispatch();
-
     const location = useLocation();
 
     const { productData } = useSelector(state => state.user);
 
     React.useEffect(() => {
         dispatch(getProducts());
-    }, [dispatch])
+    }, [dispatch]);
 
     const [anchorEl, setAnchorEl] = React.useState(null);
-
     const open = Boolean(anchorEl);
 
     const handleClick = (event) => {
@@ -35,25 +31,26 @@ const ProductsMenu = ({ dropName }) => {
         setAnchorEl(null);
     };
 
-    const uniqueItems = productData.filter((data, index, self) => {
-        return (dropName === "Categories" ?
-            self.findIndex((item) => item.category === data.category) === index :
-            self.findIndex((item) => item.subcategory === data.subcategory) === index
-        );
-    });
+    // Guard: check productData is an array before filter
+    const uniqueItems = Array.isArray(productData)
+        ? productData.filter((data, index, self) => {
+            return dropName === "Categories"
+                ? self.findIndex(item => item.category === data.category) === index
+                : self.findIndex(item => item.subcategory === data.subcategory) === index;
+        })
+        : [];
 
     const catHandler = (key) => {
         setAnchorEl(null);
         if (dropName === "Categories") {
             dispatch(getSearchedProducts("searchProductbyCategory", key));
-        }
-        else {
+        } else {
             dispatch(getSearchedProducts("searchProductbySubCategory", key));
         }
         if (location.pathname !== "/ProductSearch") {
             navigate("/ProductSearch");
         }
-    }
+    };
 
     return (
         <div style={{ marginLeft: "2rem" }}>
@@ -68,43 +65,28 @@ const ProductsMenu = ({ dropName }) => {
             >
                 {dropName}
             </LightPurpleButton>
-            {
-                dropName === "Categories" ?
-                    <StyledMenu
-                        id="demo-customized-menu"
-                        MenuListProps={{
-                            'aria-labelledby': 'demo-customized-button',
+
+            <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{ 'aria-labelledby': 'demo-customized-button' }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+            >
+                {uniqueItems.map(data => (
+                    <MenuItem
+                        onClick={() => {
+                            catHandler(dropName === "Categories" ? data.category : data.subcategory);
                         }}
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
+                        key={data._id || (dropName === "Categories" ? data.category : data.subcategory)}
                     >
-                        {uniqueItems.map((data) => (
-                            <MenuItem onClick={() => { catHandler(data.category) }} key={data._id}>
-                                {data.category}
-                            </MenuItem>
-                        ))}
-                    </StyledMenu>
-                    :
-                    <StyledMenu
-                        id="demo-customized-menu"
-                        MenuListProps={{
-                            'aria-labelledby': 'demo-customized-button',
-                        }}
-                        anchorEl={anchorEl}
-                        open={open}
-                        onClose={handleClose}
-                    >
-                        {uniqueItems.map((data) => (
-                            <MenuItem onClick={() => { catHandler(data.subcategory) }} key={data._id}>
-                                {data.subcategory}
-                            </MenuItem>
-                        ))}
-                    </StyledMenu>
-            }
+                        {dropName === "Categories" ? data.category : data.subcategory}
+                    </MenuItem>
+                ))}
+            </StyledMenu>
         </div>
     );
-}
+};
 
 export default ProductsMenu;
 
@@ -129,7 +111,8 @@ const StyledMenu = styled((props) => (
         color:
             theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
         boxShadow:
-            'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+            'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, ' +
+            'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
         '& .MuiMenu-list': {
             padding: '4px 0',
         },
